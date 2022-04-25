5343c9ebaa368950eaf585f8ac2b0313ee9b4b9b
Rafael Santos <rafaelcs_94@hotmail.com>
2022-04-25 15:59:01 -0300

    feat: adds get method for /talker/:id endpoint

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
 const fs = require('fs');
-const { OK_STATUS } = require('../statusCode');
+const { OK_STATUS, NOT_FOUND } = require('../statusCode');
 
 router.get('/', (_req, res) => {
   const result = fs.readFileSync('./talker.json');
   return res.status(OK_STATUS).json(JSON.parse(result));
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   const { id } = req.params;
   const talkers = JSON.parse(fs.readFileSync('./talker.json'));
   const result = talkers.find((talker) => talker.id === Number(id));
+
+  if (!result) return next({ status: NOT_FOUND, message: 'Pessoa palestrante não encontrada' });
+  
   return res.status(OK_STATUS).json(result);
 });
 
