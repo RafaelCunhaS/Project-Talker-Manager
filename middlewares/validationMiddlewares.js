608e3f0ffaa99b7fbd350af7c0b3a66be0922eef
Rafael Santos <rafaelcs_94@hotmail.com>
2022-04-25 17:12:47 -0300

    feat: adds post method for /talker endpoint with validations

--- middlewares/validationMiddlewares.js
+++ middlewares/validationMiddlewares.js
@@ -1,4 +1,4 @@
-const { BAD_REQUEST } = require('../statusCode');
+const { BAD_REQUEST, UNAUTHORIZED } = require('../statusCode');
 
 const validateEmail = (req, _res, next) => {
   const { email } = req.body;
@@ -24,4 +24,79 @@ const validatePassword = (req, _res, next) => {
   next();
 };
 
-module.exports = { validateEmail, validatePassword };
+const validateAuthorization = (req, _res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) next({ status: UNAUTHORIZED, message: 'Token não encontrado' });
+
+  if (authorization.length !== 16) {
+    next({ status: UNAUTHORIZED, message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const validateName = (req, _res, next) => {
+  const { name } = req.body;
+
+  if (!name) next({ status: BAD_REQUEST, message: 'O campo "name" é obrigatório' });
+
+  if (name.length < 3) {
+    next({ status: BAD_REQUEST, message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validateAge = (req, _res, next) => {
+  const { age } = req.body;
+
+  if (!age) next({ status: BAD_REQUEST, message: 'O campo "age" é obrigatório' });
+
+  if (age < 18) {
+    next({ status: BAD_REQUEST, message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const validateTalk = (req, _res, next) => {
+  const { talk } = req.body;
+  const error = { status: BAD_REQUEST, 
+    message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' };
+  
+  if (!talk) {
+    next(error);
+  }
+
+  const { talk: { watchedAt, rate } } = req.body;
+
+  if (!watchedAt || !rate) {
+    next(error);
+  }
+
+  next();
+};
+
+const validateDateAndRate = (req, _res, next) => {
+  const { talk: { watchedAt, rate } } = req.body;
+
+  if (!watchedAt.match(/[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/)) {
+    next({ status: BAD_REQUEST, message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  if (rate < 1 || rate > 5) {
+    next({ status: BAD_REQUEST, message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
+module.exports = { 
+  validateEmail, 
+  validatePassword, 
+  validateAuthorization,
+  validateName, 
+  validateAge, 
+  validateTalk, 
+  validateDateAndRate };
--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const fs = require('fs');
-const { OK_STATUS, NOT_FOUND } = require('../statusCode');
+const { OK_STATUS, NOT_FOUND, CREATED } = require('../statusCode');
+const { 
+  validateAuthorization, 
+  validateName, validateAge,
+  validateTalk, validateDateAndRate } = require('../middlewares/validationMiddlewares');
 
 router.get('/', (_req, res) => {
   const result = fs.readFileSync('./talker.json');
@@ -15,6 +19,21 @@ router.get('/:id', (req, res, next) => {
   if (!result) return next({ status: NOT_FOUND, message: 'Pessoa palestrante não encontrada' });
   
   return res.status(OK_STATUS).json(result);
+});
+
+router.post('/', validateAuthorization, validateName,
+validateAge, validateTalk, validateDateAndRate, (req, res) => {
+  const obj = req.body;
+  const talkers = JSON.parse(fs.readFileSync('./talker.json'));
+
+  const lastId = talkers[talkers.length - 1].id;
+
+  const newTalker = { id: lastId + 1, ...obj };
+
+  talkers.push(newTalker);
+
+  fs.writeFileSync('./talker.json', JSON.stringify(talkers));
+  return res.status(CREATED).json(newTalker);
 });
 
 module.exports = router;
